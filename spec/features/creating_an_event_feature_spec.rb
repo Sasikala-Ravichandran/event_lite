6f17c57d5540ce0f0ef071f881d208055b4bd6bf
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-02-24 01:04:16 -0500

    Added creating an event feature spec

---
+++ spec/features/creating_an_event_feature_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "Creating an event" do
+
+  scenario do
+    user = create(:user)
+    event = build(:event)
+    
+    login_as(user)
+
+    visit new_event_path
+
+    fill_in "Name", with: event.name
+    fill_in "Start date", with: event.start_date
+    fill_in "Start time", with: event.start_time
+    fill_in "Venue", with: event.venue
+
+    click_button "Create Event"
+
+    expect(page).to have_content("Event has been created")
+  end
+
+end
